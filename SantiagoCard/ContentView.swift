ab67509f7499aad92c518cdb5595154ce83bf0b2
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-19 09:31:46 -0300

    Initial Commit

---
+++ SantiagoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SantiagoCard
+//
+//  Created by Santiago Rodriguez Affonso on 19/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SantiagoCard/SantiagoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantiagoCardApp.swift
+//  SantiagoCard
+//
+//  Created by Santiago Rodriguez Affonso on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SantiagoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
